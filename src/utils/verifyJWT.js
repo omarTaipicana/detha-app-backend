ea7dc20dc603cf38e5e27395d2c70b0d8093aff3
Fernando <fernandoparadoja@gmail.com>
2024-12-11 16:46:23 -0500

    encadenamiento verify

--- src/utils/verifyJWT.js
+++ src/utils/verifyJWT.js
@@ -3,7 +3,7 @@ require('dotenv').config();
 
 const verifyJWT = (req, res, next) => {
     const authHeader = req.headers.authorization || req.headers.Authorization;
-    if (!authHeader?.startsWith('Bearer ')) return res.sendStatus(401);
+    if (!authHeader || !authHeader.startsWith('Bearer ')) return res.sendStatus(401);
     const token = authHeader.split(' ')[1]; 
     jwt.verify(
         token,
